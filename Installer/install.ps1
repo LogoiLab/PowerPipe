c8da45960d86de83ad8d227abf01cef88929d271
LogoiLab <mistercbax@gmail.com>
2015-10-06 20:18:43 -0400

    made installer PSV2 compatible

--- Installer/install.ps1
+++ Installer/install.ps1
@@ -1,9 +1,9 @@
-#requires -Version 3
+#requires -Version 2
 $ModuleHome = Join-Path -Path ($Env:PSModulePath.split(';')[0]) -ChildPath '\PowerPipe'
 New-Item -Path $ModuleHome -ItemType Directory -Force
 New-Item -Path (Join-Path -Path $ModuleHome -ChildPath '\PipeStorage') -ItemType Directory -Force
 New-Item -Path (Join-Path -Path $ModuleHome -ChildPath '\Source') -ItemType Directory -Force
 $ModuleSource = 'https://raw.githubusercontent.com/LogoiLab/PowerPipe/master/PowerPipe.psm1'
 $ModuleManifest = 'https://raw.githubusercontent.com/LogoiLab/PowerPipe/master/PowerPipe.psd1'
-Invoke-WebRequest -Uri $ModuleSource -OutFile (Join-Path -Path $ModuleHome -ChildPath '\Source\PowerPipe.psm1')
-Invoke-WebRequest -Uri $ModuleManifest -OutFile (Join-Path -Path $ModuleHome -ChildPath '\Source\PowerPipe.psd1')
+(New-Object System.Net.WebClient).DownloadFile($ModuleSource,(Join-Path -Path $ModuleHome -ChildPath '\Source\PowerPipe.psm1'))
+(New-Object System.Net.WebClient).DownloadFile($ModuleManifest,(Join-Path -Path $ModuleHome -ChildPath '\Source\PowerPipe.psd1'))
